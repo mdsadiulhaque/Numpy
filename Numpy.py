5c3e3d2fe2a9e2633b7eb7cad82a5c1a6efb78bf
Md Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-06-21 12:31:34 +0600

    Update Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+#creating arrays
+
+print (np.zeros(10, dtype='int'))
+
+#creating a 3 row x 5 column matrix
+
+print(np.ones((3,5), dtype=float))
+
+#creating a matrix with a predefined value
+
+print(np.full((3,5),1.23))
